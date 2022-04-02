e8873818bae3c4d862590066fb4a1ff31ae50718
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2022-04-02 20:29:06 -0300

    Adicionando arquivos iniciais
    Adicionando arquivos iniciais

---
+++ src/Banco.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class Banco {
+    private String nome;
+    private List<Conta> contas;
+
+    public Banco() {
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Conta> getContas() {
+        return this.contas;
+    }
+
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
+    }
+}
---
+++ src/Cliente.java
@@ -0,0 +1,14 @@
+public class Cliente {
+    private String nome;
+
+    public Cliente() {
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
